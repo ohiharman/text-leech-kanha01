0e8a6acb3e180bab94b1a1822b47bc9f0758d361
Soninkuwal <158008154+Soninkuwal@users.noreply.github.com>
2024-11-15 14:47:19 +0530

    Update vars.py

--- modules/vars.py
+++ modules/vars.py
@@ -2,6 +2,6 @@
 #https://t.me/Sonickuwalupdate
 # Add your details here and then deploy by clicking on HEROKU Deploy button
 
-api_id = "20945078"
-api_hash = "93f6b8ce4bb0ab61b4c7e42187f2aa64"
-bot_token = "6298609764:AAHZWQcHTq5DlmRH1TvBCRE9_gxW_5UqXnk"
+api_id = ""
+api_hash = ""
+bot_token = ""
